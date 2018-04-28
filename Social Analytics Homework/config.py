7f1e37fec1a1286129f10c6a9955c94873118e34
dcates <35199013+dcates@users.noreply.github.com>
2018-04-28 16:10:16 -0500

    Update config.py

--- Social Analytics Homework/config.py
+++ Social Analytics Homework/config.py
@@ -1,5 +1,5 @@
 # Twitter API Keys
-consumer_key = "6gle0A7idLJ6buHExczFkcITn"
-consumer_secret = "EfUO9dEERYvqVArOkX8FHMm316QABh8Hfjdf6VJUXpnju1NRrq"
-access_token = "751039949382746112-u6XL2EE9ANEzERwNI8stFtIyxQ776W0"
-access_token_secret = "uKUfmpPYUnNATuNEyM9qervAzQi4LxJ9Ns5D24ZlCdNOz"
+consumer_key = "insert here"
+consumer_secret = "insert here"
+access_token = "insert insert here"
+access_token_secret = "insert here"
